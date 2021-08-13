e266d1900b6f055537624ef58ebf65744a35d810
Maria del Pilar Sanchez <sanchezpili6@Marias-MacBook-Pro.local>
2021-08-12 19:00:50 -0500

    Tarea parentesis

---
+++ parentesis.py
@@ -0,0 +1,30 @@
+# Recibes una string conteniendo solo () y []
+# Crea un algoritmos para validar que la sintaxis de los corchetes es correcta:
+# Ejemplos validos:
+# '()'
+# '()()()()()()()()()'
+# '([])[]()'
+# Ejemplos no validos:
+# ')'
+# '()['
+# '(([))]'
+# '(((((((((((((((((((((((((((((((((('
+
+VALID_PARENTHESES_STRING = '()()()()()()()()()'
+INVALID_PARENTHESES_STRING = '())](())[]'
+OPENING_CHARS, CLOSING_CHARS = '([', ')]'
+
+
+def parentheses(parentheses_string):
+    if not parentheses_string:
+        return True
+    for i, character in enumerate(parentheses_string):
+        if character in CLOSING_CHARS:
+            index = CLOSING_CHARS.index(character)
+            if parentheses_string[i-1] != OPENING_CHARS[index]:
+                return False
+            return parentheses(parentheses_string[:i-1] + parentheses_string[i+1:])
+
+
+print(parentheses(VALID_PARENTHESES_STRING))
+print(parentheses(INVALID_PARENTHESES_STRING))
