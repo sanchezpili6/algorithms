d42c3cd92e527eab69c66a62733ecde4fb0e4bf8
Pili Sánchez <sanchezpili6@gmail.com>
2021-11-11 00:57:04 -0600

    Agregar estaciones adjacentes a diccionario

--- Graphs/distance_graph.py
+++ Graphs/distance_graph.py
@@ -1,49 +1,123 @@
 from vertex import Vertex
 from graph import Graph
 
-# definir al grafo Metro
 metro = Graph()
-
-# definir estaciones
-
 estaciones = {
-    'rosario': 'El Rosario',
-    'instituto_del_petroleo': 'Instituto del Petroleo',
-    'deportivo_18': 'Deportivo 18 de Marzo',
-    'martin_carrera': 'Martín Carrera',
-    'la_raza': 'La Raza',
-    'consulado': 'Consulado',
-    'tacuba': 'Tacuba',
-    'guerrero': 'Guerrero',
-    'garibaldi': 'Garibaldi',
-    'oceania': 'Oceanía',
-    'hidalgo': 'Hidalgo',
-    'bellas_artes': 'Bellas Artes',
-    'morelos': 'Morelos',
-    'balderas': 'Balderas',
-    'salto_del_agua': 'Salto del Agua',
-    'pino_suarez': 'Pino Suarez',
-    'san_lazaro': 'San Lázaro',
-    'candelaria': 'Candelaria',
-    'tacubaya': 'Tacubaya',
-    'centro_medico': 'Centro Médico',
-    'chabacano': 'Chabacano',
-    'jamaica': 'Jamaica',
-    'santa_anita': 'Santa Anita',
-    'mixcoac': 'Mixcoac',
-    'zapata': 'Zapata',
-    'ermita': 'Ermita',
-    'atlalilco': 'Atlalilco',
-    'pantitlan': 'Pantitlán',
+    'rosario': {
+        'name': 'El Rosario',
+        'adjacent': ['instituto_del_petroleo', 'tacuba']
+    },
+    'instituto_del_petroleo': {
+        'name': 'Instituto del Petroleo',
+        'adjacent': ['la_raza', 'deportivo_18']
+    },
+    'deportivo_18': {
+        'name': 'Deportivo 18 de Marzo',
+        'adjacent': ['la_raza', 'martin_carrera']
+    },
+    'martin_carrera': {
+        'name': 'Martín Carrera',
+        'adjacent': ['consulado']
+    },
+    'la_raza': {
+        'name': 'La Raza',
+        'adjacent': ['guerrero', 'consulado']
+    },
+    'consulado': {
+        'name': 'Consulado',
+        'adjacent': ['morelos', 'oceania']
+    },
+    'tacuba': {
+        'name': 'Tacuba',
+        'adjacent': ['hidalgo', 'tacubaya']
+    },
+    'guerrero': {
+        'name': 'Guerrero',
+        'adjacent': ['garibaldi', 'hidalgo']
+    },
+    'garibaldi': {
+        'name': 'Garibaldi',
+        'adjacent': ['bellas_artes', 'morelos']
+    },
+    'oceania': {
+        'name': 'Oceanía',
+        'adjacent': ['san_lazaro', 'pantitlan']
+    },
+    'hidalgo': {
+        'name': 'Hidalgo',
+        'adjacent': ['bellas_artes', 'balderas']
+    },
+    'bellas_artes': {
+        'name': 'Bellas Artes',
+        'adjacent': ['salto_del_agua', 'pino_suarez']
+    },
+    'morelos': {
+        'name': 'Morelos',
+        'adjacent': ['san_lazaro', 'candelaria']
+    },
+    'balderas': {
+        'name': 'Balderas',
+        'adjacent': ['salto_del_agua', 'centro_medico', 'tacubaya']
+    },
+    'salto_del_agua': {
+        'name': 'Salto del Agua',
+        'adjacent': ['pino_suarez', 'chabacano']
+    },
+    'pino_suarez': {
+        'name': 'Pino Suarez',
+        'adjacent': ['candelaria', 'chabacano']
+    },
+    'san_lazaro': {
+        'name': 'San Lázaro',
+        'adjacent': ['candelaria', 'pantitlan']
+    },
+    'candelaria': {
+        'name': 'Candelaria',
+        'adjacent': ['jamaica']
+    },
+    'tacubaya': {
+        'name': 'Tacubaya',
+        'adjacent': ['centro_medico', 'mixcoac']
+    },
+    'centro_medico': {
+        'name': 'Centro Médico',
+        'adjacent': ['chabacano', 'zapata']
+    },
+    'chabacano': {
+        'name': 'Chabacano',
+        'adjacent': ['jamaica', 'santa_anita', 'ermita']
+    },
+    'jamaica': {
+        'name': 'Jamaica',
+        'adjacent': ['santa_anita', 'pantitlan']
+    },
+    'santa_anita': {
+        'name': 'Santa Anita',
+        'adjacent': ['atlalilco']
+    },
+    'mixcoac': {
+        'name': 'Mixcoac',
+        'adjacent': ['zapata']
+    },
+    'zapata': {
+        'name': 'Zapata',
+        'adjacent': ['ermita']
+    },
+    'ermita': {
+        'name': 'Ermita',
+        'adjacent': ['atlalilco']
+    },
+    'atlalilco': {
+        'name': 'Atlalilco',
+        'adjacent': []
+    },
+    'pantitlan': {
+        'name': 'Pantitlán',
+        'adjacent': []
+    }
 }
 
-# crear vertices por estaciones
-
 for key, value in estaciones.items():
-    key = Vertex(value)
-
-# Agregar vértices de estaciones al grafo metro
-
-for key, value in estaciones.items():
-    print(key)
+    key = Vertex(value['name'])
     metro.add_vertex(key)
+
--- Graphs/graph.py
+++ Graphs/graph.py
@@ -7,6 +7,11 @@ class Graph:
         self.graph_dict = {}
 
     def add_vertex(self, vertex):
-        print('Vertex ' + vertex + ' added')
+        print('Vertex ' + vertex.value + ' added')
         self.graph_dict[vertex.value] = vertex
 
+    def add_edge(self, from_vertex, to_vertex):
+        print("Adding edge from " + from_vertex.value + " to " + to_vertex.value)
+        self.graph_dict[from_vertex.value].add_edge(to_vertex.value)
+        if not self.directed:
+            self.graph_dict[to_vertex.value].add_edge(from_vertex.value)
